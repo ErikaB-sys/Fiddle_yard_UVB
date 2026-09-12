41565e8f8abb33fd6d7a507de82717092d3f3f89
ERI <71966592+ErikaB-sys@users.noreply.github.com>
2026-09-12 17:55:19 +0200

    Refactor UART protocol definitions
    This commit restructures the UART command/response protocol by introducing typed command and response tables, explicit command IDs, and updated status values. It also modernizes the UART API to a context-based design with dedicated receive/decode/send routines, replacing the older ad hoc interface and aligning the code with the protocol document.
    Feat: #13 #3 #7

--- FY_Controller_Motor/include/Protokoll.h
+++ FY_Controller_Motor/include/Protokoll.h
@@ -6,68 +6,133 @@
 // It defines the structure of the messages exchanged between the controller and other components.
 // Only for Uart communication, not for I2C or SPI.
 
+
+// ---------------------------------------------------------
+// Command types
+// ---------------------------------------------------------
+
+enum class CommandType : uint8_t
+{
+    IMMEDIATE,
+    EXECUTE,
+    PRIORITY
+};
+
+// ---------------------------------------------------------
+// Command definition
+// ---------------------------------------------------------
+
+struct CommandDefinition
+{
+    uint8_t id;
+    CommandType type;
+    uint8_t telegramLength;  //  telegramLength = DataLength 
+    uint8_t response;
+  
+};
+
+// ---------------------------------------------------------
+// Response Definition
+// ---------------------------------------------------------
+
+struct ResponseDefinition
+{
+    uint8_t id;
+    uint8_t length;
+};
+// ---------------------------------------------------------
+// Command IDs
+// ---------------------------------------------------------
+
+// ---------------------------------------------------------
 //define the commands and responses for the communication protocol
+// ---------------------------------------------------------
+
+
+const CommandDefinition commandDefinitions[] =
+{    // Command,CommandType,Length CMD, Response,length Response
+    { CMD_GET_STATUS    ,  CommandType::IMMEDIATE, 1, STATUS_System },
+    { CMD_GET_ERROR     , CommandType:: IMMEDIATE, 1, STATUS_Error },
+    { CMD_GET_POSITION  , CommandType:: IMMEDIATE, 1, STATUS_Position }, 
+    { CMD_GET_TRACK     , CommandType:: IMMEDIATE, 1, STATUS_Track},
+    { CMD_HELP          , CommandType:: IMMEDIATE, 1, STATUS_Help }, 
+    { CMD_REFERENCE     , CommandType:: EXECUTE,   1, STATUS_Reference },
+    { CMD_SET_SPEED     , CommandType:: EXECUTE,   3, STATUS_Motor }, 
+    { CMD_GO            , CommandType:: EXECUTE,   1, STATUS_ACK   },
+    { CMD_LEFT          , CommandType:: EXECUTE,   1, STATUS_ACK   },   
+    { CMD_RIGHT         , CommandType:: EXECUTE,   1, STATUS_ACK   }, 
+    { CMD_SET_POSITION  , CommandType:: EXECUTE,   1, STATUS_ACK   }, 
+    { CMD_SET_TRACK     , CommandType:: EXECUTE,   1, STATUS_ACK   }, 
+    { CMD_SET_REMOTE    , CommandType:: EXECUTE,   1, STATUS_CMD   }, 
+    { CMD_SET_LOCAL     , CommandType:: EXECUTE,   1, STATUS_CMD   },   
+    { CMD_STOPP         , CommandType:: PRIORITY,  1, STATUS_System}
+  
+};
+
+const ResponseDefinition ResponseDefinitions[] =
+{
+{ STATUS_Alive  , 0   },
+{ STATUS_Error ,  4  },
+{ STATUS_System ,  2  },
+{ STATUS_CMD ,   1 },
+{ STATUS_Position ,   3 },
+{ STATUS_Reference , 1   },
+{ STATUS_Track,   3 },
+{ STATUS_Motor, 3   },
+{ STATUS_ACK,  1  },
+{ STATUS_Help, 1  }  // ! help send a long String 
+};
+
+
 
 //received commands from Uart are stored in a struct and processed by the main loop
 #define MAX_COMMAND_LENGTH  8         // Maximum length of a command received from Uart in Bytes
-#define CMD_GET_STATUS      0x19        // Command to get the status of the motor
-#define CMD_GET_ERROR       0x1A        // Command to get the current error of the FY-Controller
 
-#define CMD_REFERENCE       0x16        // Command to reference the motor position
-#define CMD_SET_SPEED       0x2B        // Command to set the speed of the motor
-#define CMD_STOPP           0x01        // Command to stop the motor
-#define CMD_GO              0x07        // Command to start the motor   
-#define CMD_LEFT            0x0B        // Command to move the motor left
-#define CMD_RIGHT           0x0D        // Command to move the motor right
+constexpr uint8_t CMD_GET_STATUS   =   0x19   ;    // Command to get the status of the motor
+constexpr uint8_t CMD_GET_ERROR    =   0x1A   ;    // Command to get the current error of the FY-Controller
 
-#define CMD_SET_POSITION    0x13        // Command to set the target position of the motor
-#define CMD_GET_POSITION    0x15        // Command to get the current position of the motor
+constexpr uint8_t CMD_REFERENCE    =   0x16   ;    // Command to reference the motor position
+constexpr uint8_t CMD_SET_SPEED    =   0x2B   ;    // Command to set the speed of the motor
 
-#define CMD_SET_TRACK       0x1C        // Command to set the track number of the motor
-#define CMD_GET_TRACK       0x23        // Command to get the current track number of the motor
-#define CMD_HELP            0x25        // Command to get the list of available commands
 
-#define CMD_SET_LOCAL       0x26        // Command to set the controller to local mode
-#define CMD_SET_REMOTE      0x2A        // Command to set the controller to remote mode
+constexpr uint8_t CMD_STOPP        =   0x01   ;    // Command to stop the motor
+constexpr uint8_t CMD_GO           =   0x07   ;    // Command to start the motor   
+constexpr uint8_t CMD_LEFT         =   0x0B   ;    // Command to move the motor left
+constexpr uint8_t CMD_RIGHT        =   0x0D   ;    // Command to move the motor right
+
+constexpr uint8_t CMD_SET_POSITION =   0x13   ;    // Command to set the target position of the motor
+constexpr uint8_t CMD_GET_POSITION =   0x15   ;    // Command to get the current position of the motor
+
+constexpr uint8_t CMD_SET_TRACK    =   0x1C   ;    // Command to set the track number of the motor
+constexpr uint8_t CMD_GET_TRACK    =   0x23   ;    // Command to get the current track number of the motor
+constexpr uint8_t CMD_HELP         =   0x25   ;    // Command to get the list of available commands
+
+constexpr uint8_t CMD_SET_LOCAL    =   0x26   ;    // Command to set the controller to local mode
+constexpr uint8_t CMD_SET_REMOTE   =   0x2A   ;    // Command to set the controller to remote mode
 
 
 
 // Send Status to Uart
-#define MAX_RESPONSE_LENGTH         8          // Maximum length of a response sent to Uart in Bytes
+#define MAX_RESPONSE_LENGTH        4             // Maximum length of a response sent to Uart in Bytes
 //general Response Codes
-#define STATUS_Alive                0x03        // Status indicating that the controller is alive Cyclically send this status to Uart every ???ms
+constexpr uint8_t STATUS_Alive                  =  0x03  ;     // Status indicating that the controller is alive Cyclically send this status to Uart every ???ms
 //resonse  for CMD_GET_ERROR 
-#define STATUS_UnknownStatus        0xF0        // Status indicating that an unknown status has been sent to Uart
-#define STATUS_UnknownCommand       0xF3        // Status indicating that an unknown command was received from Uart
-#define STATUS_Alert_border         0xF5         // Status indicating that the border is unexpected reached.
-#define STATUS_Reference_Error      0xF6        // Status indicating that an reference error has been occurred in the controller.
-#define STATUS_Error                0xF9        // Status indicating that an error has occurred in the controller will also sent  
-                                                // if  GET_Status is called and the controller is in error state. 
-#define STATUS_UnknownError         0xFF        // Status indicating that an unknown error has occurred in the controller
-
-//response for CMD_GET_STATUS
-#define STATUS_INIT                 0x00        // Status indicating that the controller is initializing
-#define STATUS_System_Idle          0x01        // Status indicating that the system is idle
-#define STATUS_MotorMoving          0x02        // Status indicating that the motor is moving
-#define STATUS_MotorStopped         0x04        // Status indicating that the motor is stopped
-
-
-//response for CMD_GET_POSITION in case refrence has not been done yet the response will be STATUS_ReferenceNotDONE
-#define STATUS_PositionReached      0x05        // Status indicating that the target position has been reached
-#define STATUS_PositionNotReached   0x06        // Status indicating that the target position has not been reached
-#define STATUS_Position             0x07        // Status indicating the current position
-//response for CMD_REFERENCE
-#define STATUS_ReferenceDONE        0x09        // Status indicating that the reference has be done successfully
-#define STATUS_ReferenceNotDONE     0x0A        // Status indicating that the reference has not been done yet or not successfully
-//response for CMD_GET_TRACK /CMD_SET_TRACK
-#define STATUS_TrackSet             0x10        // Status indicating that the track number has been set
-#define STATUS_TrackNotSet          0x11        // Status indicating that the track number has not been set
-#define STATUS_TrackNumber          0x17        // Status indicating the current track number 
-#define STATUS_TrackNotReached      0x1B        // Status indicating that the track number has not been reached yet
-
+constexpr uint8_t STATUS_Error                  =  0xF0  ;     // Sending system Status byte and 3 Byte indicating that an error has occurred. The 3 Byte following up  will tell the Modul error Byte
+//response for CMD_GET_STATUS;
+constexpr uint8_t  STATUS_System                =  0x10;
 //response for CMD_SET_LOCAL / CMD_SET_REMOTE
-#define STATUS_LocalMode            0x12        // Status indicating that the controller is in local mode
-#define STATUS_RemoteMode           0x14        // Status indicating that the controller is in remote mode
+constexpr uint8_t  STATUS_CMD                   =  0x15;
+
+
+//response for CMD_GET_POSITION in case ref=ence has not been done yet the response will be STATUS_ReferenceNotDONE
+constexpr uint8_t STATUS_Position               =  0x20    ;   // Status ID Position Information
+//response for CMD_REFERENCE
+constexpr uint8_t STATUS_Reference              =  0x30   ;    // Status ID  Reference Information
+//response for CMD_GET_TRACK /CMD_SET_TRACK=
+constexpr uint8_t STATUS_Track                  =  0x40    ;   // Status ID  Track informatiom
+constexpr uint8_t STATUS_Motor                  =  0x50;    //Status ID Motor information
+constexpr uint8_t STATUS_ACK                    =  0x60;     // Answer on mosving Commands 
+
 //response for CMD_HELP
-#define STATUS_Help                 0x18        // Status indicating that the list of available commands has been sent to Uart
+constexpr uint8_t STATUS_Help                   =  0x18   ;    // Status indicating that the list of available commands has been sent to Uart
 
--- FY_Controller_Motor/include/UART.h
+++ FY_Controller_Motor/include/UART.h
@@ -1,25 +1,31 @@
 #pragma once
+
 #include <Arduino.h>
-#include <stdio.h>
 
+struct UART_Context
+{
+    uint8_t*  status;
+    uint8_t*  error;
+    int32_t*  position;
+    uint8_t*  track;
+    uint16_t* speed;
 
-class UART {
-public:
-    UART(const char* device, int baud);
-    ~UART();
-
-    bool begin(unsigned long B_rate); 
-    uint8_t update(uint8_t *buffer,size_t length  );
- // special function to send help message over Uart
-    bool send_help();
-    bool send_Error(uint8_t* error_code);
-
-private:
-    int baud_rate{0};
-    int uart_fd{-1};
-    const char* uart_device{nullptr};
-    bool sendData(const uint8_t* data, size_t length);
-    bool receiveData(uint8_t* buffer, size_t length);
-  
 };
 
+class UART
+{
+public:
+    UART();
+
+    void begin(UART_Context& context);
+    void update();
+
+private:
+
+    UART_Context* context;
+
+    void receive();
+    void decodeCommand();
+    void sendResponse();
+    void sendHello();
+};
