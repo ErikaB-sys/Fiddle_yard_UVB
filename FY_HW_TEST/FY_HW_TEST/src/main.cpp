83ff0d2f5b6393ae4493c723fb461f3a6e3417a6
ERI <71966592+ErikaB-sys@users.noreply.github.com>
2026-09-20 20:57:51 +0200

    TEST: add I2C scanner to HW test initialization

--- FY_HW_TEST/FY_HW_TEST/src/main.cpp
+++ FY_HW_TEST/FY_HW_TEST/src/main.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
-
-
+#include <Wire.h>
 
 // LED Test für Testplatine
 // D2, D3, D4 + Built-in LED
@@ -10,13 +9,47 @@ const uint8_t LED2 = 3;
 const uint8_t LED3 = 4;
 const uint8_t LED_BUILTIN_PIN = LED_BUILTIN;
 
+void scanI2C()
+{
+  Serial.println(F("I2C Scanner"));
+  uint8_t found = 0;
+
+  for (uint8_t address = 1; address < 127; address++)
+  {
+    Wire.beginTransmission(address);
+
+    if (Wire.endTransmission() == 0)
+    {
+      Serial.print(F("I2C device: 0x"));
+      if (address < 0x10)
+        Serial.print('0');
+      Serial.println(address, HEX);
+      found++;
+    }
+  }
+
+  if (found == 0)
+    Serial.println(F("No I2C devices found"));
+
+  Serial.print(F("I2C devices found: "));
+  Serial.println(found);
+}
+
 void setup()
 {
+  Serial.begin(115200);
+
+  Wire.begin();
+  delay(100);
+
+  scanI2C();
+
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
   pinMode(LED3, OUTPUT);
   pinMode(LED_BUILTIN_PIN, OUTPUT);
 }
+
 void setPWM(uint16_t frequency)
 {
   // Timer2 CTC, Prescaler 64
@@ -36,32 +69,39 @@ void setPWM(uint16_t frequency)
   TCCR2A = (1 << WGM21) | (1 << COM2B1);
   TCCR2B = (1 << CS22);
 }
+
 #define F_min 65
-uint16_t PWM=F_min; 
+uint16_t PWM = F_min;
+
 void loop()
-{ 
- if (PWM <300)
- {PWM+=10;}
-else 
- {PWM=F_min;}
+{
+  if (PWM < 300)
+  {
+    PWM += 10;
+  }
+  else
+  {
+    PWM = F_min;
+  }
+
   digitalWrite(LED1, LOW);
   digitalWrite(LED3, LOW);
   setPWM(PWM);
   digitalWrite(LED_BUILTIN_PIN, LOW);
-     delay(1000 / PWM * 10);
+  delay(1000 / PWM * 10);
 
   digitalWrite(LED1, LOW);
   digitalWrite(LED3, HIGH);
   digitalWrite(LED_BUILTIN_PIN, LOW);
-   delay(1000 / PWM * 10);
+  delay(1000 / PWM * 10);
 
   digitalWrite(LED1, HIGH);
   digitalWrite(LED3, LOW);
   digitalWrite(LED_BUILTIN_PIN, LOW);
-     delay(1000 / PWM * 10);
+  delay(1000 / PWM * 10);
 
   digitalWrite(LED1, HIGH);
   digitalWrite(LED3, HIGH);
   digitalWrite(LED_BUILTIN_PIN, HIGH);
-    delay(1000 / PWM * 10);
+  delay(1000 / PWM * 10);
 }
