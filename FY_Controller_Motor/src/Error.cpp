be7b0e60ea03bd6a32b5edae51a1725359316acc
ERI <71966592+ErikaB-sys@users.noreply.github.com>
2026-09-21 07:34:50 +0200

    M6: add central error state implementation

---
+++ FY_Controller_Motor/src/Error.cpp
@@ -0,0 +1,26 @@
+#include "Error.h"
+
+namespace
+{
+    uint8_t currentError = FY_ERROR_NONE;
+}
+
+void setError(uint8_t errorCode)
+{
+    currentError = errorCode;
+}
+
+uint8_t getError()
+{
+    return currentError;
+}
+
+bool hasError()
+{
+    return currentError != FY_ERROR_NONE;
+}
+
+void clearError()
+{
+    currentError = FY_ERROR_NONE;
+}
